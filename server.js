7ae862446ce500f22ded55cf83abce756d8ef44c
Nick Martin <martin.nicholas.r@gmail.com>
2022-03-15 22:36:52 -0600

    adding 404 error page and route

--- server.js
+++ server.js
@@ -15,12 +15,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/api', api);
 
-// Question for TA or Tutor - why do I need to use /api for getting the data -- and why does it not like it 
-// when I try to serve my HTML page from the same place the data is being called. Does it have something to do
-// with the following line of code? Just kind of want to get an idea of best practices in organizing what routes where
-// Is it confusing to the sevrer if I have /notes serving both data and a static page
-// The front-end fetch has api in the path and just kind of need  a refresher on how that's talking to each other
-// Cant Insomia hit the endpoint where the data is on it's own?
 app.use(express.static('public'));
 
 // GET Route for homepage
@@ -35,7 +29,7 @@ app.get('/notes', (req, res) =>
 
 // GET Route for 404 
 app.get('/*', (req, res) => 
-  res.status(404).sendFile(path.join(__dirname, '/public/pages/404.html'))
+  res.status(404).sendFile(path.join(__dirname, '/public/404.html'))
 );
 
 app.listen(PORT, () =>
